36b0977efb8afab22ca94eda3191e16eeec4987a
Zeinab83 <zr138324@gaail.com>
2024-11-27 13:45:58 +0330

    first

---
+++ Area.cs
@@ -0,0 +1,15 @@
+using System.Security.Cryptography.X509Certificates;
+
+public class Area
+{
+    public void _area()
+    {
+        Console.WriteLine("شعاع:");
+        var x =double.Parse( Console.ReadLine());
+        double area1 =3.14*(x*x);
+        Console.WriteLine(area1);
+    }
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+// See https://aka.ms/new-console-template for more information
+var obj = new Area();
+obj._area();
